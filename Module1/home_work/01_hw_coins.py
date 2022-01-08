0aba2538f4d83b7a59c9eddf07d302e9f8ae7b7f
OliMoroz <96371279+OliMoroz@users.noreply.github.com>
2022-01-08 23:10:37 +0300

    Update 01_hw_coins.py

--- Module1/home_work/01_hw_coins.py
+++ Module1/home_work/01_hw_coins.py
@@ -1,21 +1,50 @@
 import random
 
-
 class Coin:
-    def __init__(self):
+    def __init__(self, side, heads, tails):
         # heads-орел/tails-решка
         self.side = None
+        self.heads = heads #орел
+        self.tails = tails #решка
+
 
     def flip(self):
-        """
-        Подбрасывание монетки
-        """
-        self.side = ...  # random: heads/tails
+        #print(self.side)
+        #for _ in range(n):
+        a = int(random.randint(1, 10000))
+        if a % 2 == 0:
+            return self.heads
+            #print(self.heads)
+            #i += 1
+        else:
+            return self.tails
+            #print(self.tails)
+            #j += 1
+        #
+        # if n == n:
+        #     return ''
 
 
-# Задание: создайте список из n-монеток. Подбросьте(flip) все монетки.
-# выведите соотношение выпавших орлов и решек в процентах
+            #print(self.heads) if a % 2 == 0 else print(self.tails)
+h = 0
+t = 0
+n = int(input('Введите число: '))
+coin1 = Coin('None', 'heads', 'tails')
+coin_l = []
+for i in range(n):
+    coin_l.append(coin1.side)
+print('Cписок с какими-то монетами - ', coin_l)
 
-# Пояснение: когда вы создаете монетку, то она находится в неопределенном состоянии self.side = None, т.е.
-# она находится у вас в руке и не выпала ни орлом ни решкой. Монетка "определеяется" со стороной(орел/решка),
-# только после того, как вы ее подбрасываете(вызываете метод flip())
+for i in range(0, len(coin_l)):
+    if coin_l[i] == None:
+        coin_l[i] = coin1.flip()
+        if coin_l[i] == 'heads':
+            h += 1
+        else:
+            t += 1
+    i += 1
+print('Cписок с монетами после подбрасывания - ', coin_l)
+print('heads = ', round(h / n * 100, ), '%', 'tails = ', round(t / n * 100, ), '%')
+
+#print(coin1.flip())
+
